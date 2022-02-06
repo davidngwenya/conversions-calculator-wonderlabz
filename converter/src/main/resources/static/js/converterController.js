5448de4f306ebe641096e4d8547562d75ac92950
davidngwenya <84163243+davidngwenya@users.noreply.github.com>
2022-02-06 07:09:47 +0200

    Add files via upload

--- converter/src/main/resources/static/js/converterController.js
+++ converter/src/main/resources/static/js/converterController.js
@@ -1,33 +1,72 @@
 var app = angular.module('converterApp', []);
 
-app.controller("converterCtrl", function($scope, $http){
+app.controller('converterCtrl', function($scope, $http){
+
+
+
     $scope.submitTemperature = function(){
         if($scope.kelvin){
-            $http.get("http://localhost:8080/conversions/ktoc?kelvin=$scope.kelvin")
+            $http.get("http://localhost:8080/conversions/ktoc?kelvin=" + $scope.kelvin)
                 .then(function (response){
                     $scope.response = response.data},
                     function error(response){
                     $scope.postResultMessage = "Error with status:" + response.statusText;});
-            $scope.form.lbText = $scope.response;
         }
         else {
             if($scope.celsius){
-                $http.get("http://localhost:8080/conversions/ctok?celsius=$scope.celsius").then(function (response){
-                    $scope.response = response.data}, function error(response){
-                    $scope.postResultMessage = "Error with status:" + response.statusText;});
+                //var celsiusNum = parseFloat($scope.celsius).toFixed(2);
+                //var cel = $scope.celsius;
+                $http.get('http://localhost:8080/conversions/ctok?celsius=' + $scope.celsius).then(function(response){
+                    $scope.conversionResponse = response.data}, function error(response){
+                    $scope.conversionResponse = "Error with status:" + response.statusText;});
+                    //$scope.conversionResponse = "Error with status:" + response.statusText;});
                     $scope.lbText = $scope.response;
             }
         }
 
     }
 
+    $scope.submitDistance = function(){
+            if($scope.miles){
+                $http.get("http://localhost:8080/conversions/mtok?miles=" + $scope.miles).then(success, error);
+            }
+            else {
+                if($scope.kilometres){
+                    $http.get('http://localhost:8080/conversions/ktom?kilometres=' + $scope.kilometres).then(success, error);
+                }
+            }
+
+        }
+
     $scope.conversions = [];
 
     $scope.getHistory = function(){
-        $http.get("http://localhost:8080/conversions/history").then(function success(response){
-                $scope.conversions = response.data}, function error(response){
-                $scope.postResultMessage = "Error with status:" + response.statusText;});
-        }
+        $http.get("http://localhost:8080/conversions/history").then(success, error);
+    }
+
+    function refreshPageData(){
+        $http.get("http://localhost:8080/");
+    }
+
+    function success(response){
+        refreshPageData();
+        clearTempForm();
+        clearDistForm();
+    }
+
+    function error(response){
+        console.log(response.statusText);
+    }
+
+    function clearTempForm(){
+        $scope.celsius = "";
+        $scope.kelvin = "";
+    }
+
+    function clearDistForm(){
+        $scope.miles = "";
+        $scope.kilometres = "";
+    }
 
 
 });
