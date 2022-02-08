793686de24dd4c5c91b115c80abaf6eaba1f158e
davidngwenya <84163243+davidngwenya@users.noreply.github.com>
2022-02-08 20:49:57 +0200

    Spring Cloud Config Server
    A Spring-Cloud-Config-Server

---
+++ spring-cloud-config-server/src/test/java/com/wonderlabz/springcloudconfigserver/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wonderlabz.springcloudconfigserver.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
